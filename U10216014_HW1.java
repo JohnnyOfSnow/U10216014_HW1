f28240534f070f5cd0c32e9ece2fed506850314c
JavaExercise征戰 <good258johnny@yahoo.com.tw>
2014-03-01 19:40:49 +0800

    增加abstract class AbstractionDrawFunction
    根據題目要求  把class直接寫上去

--- U10216014_HW1.java
+++ U10216014_HW1.java
@@ -3,3 +3,27 @@ public class U10216014_HW1 {
 		
 	}
 }
+
+public abstract class AbstractionDrawFunction extends JPanel {
+	/** Polygon to hold the points */
+	private Polygon p = new Polygon();
+
+	protected AbstractionDrawFunction() {
+		drawFunction();
+	} 
+
+	/** Return the y-coordinate */
+	abstract double f(double x);
+
+	/** Obtain points for x-coordinates 100, 101, ..., 300 */
+	public void drawFunction() {
+		for (int x = -100; x <= 100; x++) {
+			p.addPoint(x + 200, 200 - (int)f(x));
+		}
+	}
+
+	@Override /** Draw axes, labels, and connect points */
+	protected void paintComponent(Graphics g) {
+		// To be completed by you
+	}
+}
