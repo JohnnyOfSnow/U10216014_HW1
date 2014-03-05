e791d7f07703dfd665ca4725a6383a1d01fb2d2a
JavaExercise征戰 <good258johnny@yahoo.com.tw>
2014-03-05 20:13:32 +0800

    重新了解題目意思大幅修改
    本次修改的地方:
    1. 之前是直接在程式裡給數值，這次的程式可以由使用者自行輸入
    2. 在輸入數字的地方(運用第14章的14.5程式範例)，去檢查輸入的數字是否符合程式內的需要
    3. 加入與使用者互動的對話(包含一開始程式的目的，執行程式過程中輸入完之後的提示，以及結果的敘述文字)
    4. 增加進來的程式碼，增加一些註解

--- AbstractionDrawFunction.java
+++ AbstractionDrawFunction.java
@@ -1,17 +1,88 @@
 import javax.swing.JPanel;
 import java.awt.Graphics;
 import java.awt.Polygon;
+import java.util.*;
 
 public abstract class AbstractionDrawFunction extends JPanel {
+	/** Main method */
 	public static void main(String[] args) {
-		System.out.println(new functionOne().f(2));
-		System.out.println(new functionTwo().f(Math.PI / 6));
-		System.out.println(new functionThree().f(0));
-		System.out.println(new functionFour().f(Math.PI / 4));
-		System.out.println(new functionFive().f(Math.PI / 2));
-		System.out.println(new functionSix().f(Math.PI));
-		System.out.println(new functionSeven().f(10));
+		Scanner input = new Scanner(System.in); // Create a scanner to enter data from keyboard.
+		preface();
+		int yourChoose = chooseFunction();
+		System.out.println("----------------------------------------------------------------------------");
+		System.out.print("Please enter a variable x in order to calculate the answer: ");
+		double x = input.nextDouble();
+		System.out.println("----------------------------------------------------------------------------");
+		System.out.println("You enter the variable x is: " + x);
+		getFunctionCall(yourChoose,x);
 	}
+	
+	static void getFunctionCall(int yourChoose, double x) {
+		switch(yourChoose) {
+			case 1:  System.out.println("The x * x result is " + new functionOne().f(x));
+					 break;
+			case 2:  System.out.println("The sin(x) result is " + new functionTwo().f(x));
+					 break;
+			case 3:  System.out.println("The cos(x) result is " + new functionThree().f(x));
+					 break;
+			case 4:  System.out.println("The tan(x) result is " + new functionFour().f(x));
+					 break;
+			case 5:  System.out.println("The cos(x) + 5 * sin(x) result is " + new functionFive().f(x));
+					 break;
+			case 6:  System.out.println("The 5 * cos(x) + sin(x) result is " + new functionSix().f(x));
+					 break;
+			case 7:  System.out.println("The log(x) + x * x result is " + new functionSeven().f(x));
+					 break;
+			default: System.out.println("You have the error!");
+		}
+	}
+
+	// Use to tell the user what the program do.
+	static void preface() {
+		System.out.println("----------------------------------------------------------------------------");
+		System.out.println("The program can enter a number to get the answer that you choose a function.");
+		System.out.println("----------------------------------------------------------------------------");
+		System.out.println("1. x * x");
+		System.out.println("2. sin(x)");
+		System.out.println("3. cos(x)");
+		System.out.println("4. tan(x)");
+		System.out.println("5. cos(x) + 5 * sin(x)");
+		System.out.println("6. 5 * cos(x) + sin(x)");
+		System.out.println("7. log(x) + x * x");
+		System.out.println("----------------------------------------------------------------------------");
+		System.out.print("Please enter a integer to choose what function you want to get answer: ");
+	}
+
+	// The method use to enter a integer for choosing function(and verson 1.2 add the data type check)
+	static int chooseFunction() {
+		Scanner input2 = new Scanner(System.in);
+		boolean correct = true;
+		int option = 0;
+		// The loop will stop, if user can enter a vaild number.
+		do {
+			try {
+				option = input2.nextInt();
+				/**
+			 	*  If the data type is int. Prehaps,
+				*	case1: The number is not in 1 ~ 7 , so user should enter a number again.
+				*  case2: The number is correct(data type is int, and in 1 ~ 7).
+				*/
+				if(option > 0 && option < 8){
+					correct = false;
+					System.out.println("You have decided the fuction you want to use");
+				} else {
+					System.out.print("Please a integer from 1 to 7 to choose your function: ");
+				}	
+				
+			}
+			catch (InputMismatchException ex) {
+				System.out.print("Please a integer from 1 to 7  to choose your function: ");
+				input2.nextLine(); // Discard input
+			}
+		} while(correct);
+		return option;
+	}
+	
 	/** Polygon to hold the points */
 	private Polygon p = new Polygon();
 
